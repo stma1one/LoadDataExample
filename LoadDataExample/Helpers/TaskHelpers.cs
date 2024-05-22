7a84a28f2c8fe0198e41fad5da7842fb903b4b10
User <User@LAB3-3>
2024-05-22 08:25:43 +0300

    Update Comments

--- LoadDataExample/Helpers/TaskHelpers.cs
+++ LoadDataExample/Helpers/TaskHelpers.cs
@@ -7,8 +7,23 @@ using System.Threading.Tasks;
 
 namespace LoadDataExample
 {
+
+    /*
+     * מחלקת עזר המוסיפה ל
+     * Task
+     * פעולה המאפשרת לטפל בפעולה א-סינכרונית המופעלת מפעולות סינכרוניות    * 
+     */
+
+
     public static class TaskHelpers
     {
+        /// <summary>
+        /// Extention 
+        /// המאפשר להפעיל פעולה א-סינכרונית מתוך פעולה סינכרונית- (לשימוש בפעולות בונות)
+        /// </summary>
+        /// <param name="task">המשימה שיש להפעיל</param>
+        /// <param name="completedCallback">הפעולה שיש להפעיל במקרה של הצלחה</param>
+        /// <param name="failedCallBack">פעולה שיש להפעיל במקרה של כשלון</param>
         public async static  void  Await(this Task task,Action completedCallback=null, Action<Exception> failedCallBack=null)
         {
             try
